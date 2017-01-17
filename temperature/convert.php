4cc642fa39f0e7656a3d3da33f1af0b3ce17e43c
JosephWanderer <banhstopjw@gmail.com>
2017-01-16 23:50:28 -0800

    Standardized to PSR-1 and PSR-2, some code modification
    convert.php now uses a switch statement instead of an if block (for
    readibility and editibility).
    The methods in the temperature class have been changed to static (so
    they can be used without instantiating an object).

--- temperature/convert.php
+++ temperature/convert.php
@@ -6,7 +6,12 @@
  <form action = "<?php echo $_SERVER['PHP_SELF']; ?>" method = "GET"> 
  Degrees: 
  <input type = "text" name = "degree" size=3> 
- <select name="scale"> <option value="celcius">Celsius</option> <option value="fahrenheit">Fahrenheit</option> <option value="kelvin">Kelvin</option> </select> 
+ <select name="scale">
+     <option value="celcius">Celsius</option>
+     <option value="fahrenheit">Fahrenheit</option>
+     <option value="kelvin">Kelvin</option>
+     <option value="rankine">Rankine</option>
+     </select> 
  <br/> 
  <input type = "submit" name = "Convert Temperature"/> 
  </form> 
@@ -21,45 +26,55 @@ include 'temperatureclass.php';
 	$degree = $_GET['degree'];
 	$firstLength = strlen($_GET['degree']);
 	
-     $temperature = new Temperature();
      
 	//check that input is not NULL or NaN
-	if(($firstLength > 0) && (is_numeric($_GET['degree'])))
-		{
-		if ($scale == "celcius") 
-		{
-			 print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td>$degree</td><td>celsius</td></tr>"; 
-            
-			 $temp = $temperature->c2f($degree);
-			 print "<tr><td>$temp</td><td>fahrenheit</td></tr>"; 
-            
-			 $temp = $temperature->c2k($degree);
-			 print "<tr><td>$temp</td><td>kelvin</td></tr>"; } 
+	if (($firstLength > 0) && (is_numeric($_GET['degree']))) {
         
-		if ($scale == "fahrenheit") 
-		{
-			 print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td>$degree</td><td>farhenheit</td></tr>";
-            
-			 $temp = $temperature->f2c($degree); 
-			 print "<tr><td>$temp</td><td>celsius</td></tr>";
-            
-			 $temp = $temperature->f2k($degree); 
-			 print "<tr><td>$temp</td><td>kelvin</td></tr>";} 
-		 
-		 if ($scale == "kelvin") 
-		{
-			 print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td> 
-			 $degree</td><td>kelvin</td></tr>"; 
-             
-			 $temp = $temperature->k2f($degree); 
-			 print "<tr><td>$temp</td><td>fahrenheit</td></tr>";
-             
-			 $temp = $temperature->k2c($degree); 
-			 print "<tr><td>$temp</td><td>celsius</td></tr>";} 
-		  
-	 }
-	 else
+        //switch statement to convert the input depending on scale and format it for output
+        switch ($scale) {
+            case "celcius":
+                $degC = number_format($degree, 2);
+                $degF = number_format((Temperature::c2f($degree)), 2);
+                $degK = number_format((Temperature::c2k($degree)), 2);
+                $degR = number_format((Temperature::c2r($degree)), 2);
+                break;
+            case "fahrenheit":
+                $degC = number_format((Temperature::f2c($degree)), 2);
+                $degF = number_format($degree, 2);
+                $degK = number_format((Temperature::f2k($degree)), 2);
+                $degR = number_format((Temperature::f2r($degree)), 2);
+                break;
+            case "kelvin":
+                $degC = number_format((Temperature::k2c($degree)), 2);
+                $degF = number_format((Temperature::k2f($degree)), 2);
+                $degK = number_format($degree, 2);
+                $degR = number_format((Temperature::k2r($degree)), 2);
+                break;
+            case "rankine":
+                $degC = number_format((Temperature::r2c($degree)), 2);
+                $degF = number_format((Temperature::r2f($degree)), 2);
+                $degK = number_format((Temperature::r2k($degree)), 2);
+                $degR = number_format($degree, 2);
+                break;
+            default;
+                $degC = "Error";
+                $degF = "Error";
+                $degK = "Error";
+                $degR = "Error";
+                break;
+        }
+        
+        //print a table of the conversions
+        print "<table border><tr><th colspan=2> Conversion Results</th></tr>";
+        print "<tr><td>$degC</td><td>Celsius</td></tr>";
+        print "<tr><td>$degF</td><td>Fahrenheit</td></tr>";
+        print "<tr><td>$degK</td><td>Kelvin</td></tr>";
+        print "<tr><td>$degR</td><td>Rankine</td></tr>";
+	  
+	 } else {
 		//print an error message if input is NULL or NaN
 		echo "<span style = \"color:red\">*Please Enter a Valid Temperature.</span>";
+    }
 }
  ?> 
+     
