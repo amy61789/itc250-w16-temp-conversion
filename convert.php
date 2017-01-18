38aed494db1a8d56b0023496e28785a9eb776adf
josephwanderer <banhstopjw@gmail.com>
2017-01-17 16:46:19 -0800

    Update convert.php for static methods, rankine, css

--- convert.php
+++ convert.php
@@ -1,70 +1,92 @@
 <head> 
- <title>Convert Temperature</title> 
+
+ <title>Convert Temperature</title>
     
 <link rel="stylesheet" type="text/css" href="failsafe.css">
-<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     
- </head> 
- <body> 
+ </head>
+ <body>
+     
      <div id="container">
- <h2>Temperature Conversion</h2> 
- <form action = "<?php echo $_SERVER['PHP_SELF']; ?>" method = "GET"> 
- Degrees: 
- <input type = "text" name = "degree" size=3> 
- <select name="scale"> <option value="celcius">Celsius</option> <option value="fahrenheit">Fahrenheit</option> <option value="kelvin">Kelvin</option> </select> 
- <br/> 
- <input type = "submit" name = "Convert Temperature"/>
- </form> 
+         <h2>Temperature Conversion</h2>
+         <form action = "<?php echo $_SERVER['PHP_SELF']; ?>" method = "GET">
+              Degrees: 
+             <input type = "text" name = "degree" size=3>
+             <select name="scale">
+                 <option value="celsius">Celsius</option>
+                 <option value="fahrenheit">Fahrenheit</option>
+                 <option value="kelvin">Kelvin</option>
+                 <option value="rankine">Rankine</option>
+             </select>
+             <br/>
+             <input type = "submit" name = "Convert Temperature"/>
+         </form> 
 
 <?php 
-     
 include 'temperatureclass.php';
-         
- //check for input
- if (array_key_exists('degree',$_GET)){
-	$scale = $_GET['scale'];
-	$degree = $_GET['degree'];
-	$firstLength = strlen($_GET['degree']);
-	
-    $temperature = new Temperature();
-     
-	//check that input is not NULL or NaN
-	if(($firstLength > 0) && (is_numeric($_GET['degree'])))
-		{
-		if ($scale == "celcius") 
-		{
-			 print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td>$degree</td><td>celsius</td></tr>"; 
-            
-			 $temp = $temperature->c2f($degree);
-			 print "<tr><td>$temp</td><td>fahrenheit</td></tr>"; 
-            
-			 $temp = $temperature->c2k($degree);
-			 print "<tr><td>$temp</td><td>kelvin</td></tr>"; } 
-        
-		if ($scale == "fahrenheit") 
-		{
-			 print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td>$degree</td><td>farhenheit</td></tr>";
-            
-			 $temp = $temperature->f2c($degree); 
-			 print "<tr><td>$temp</td><td>celsius</td></tr>";
-            
-			 $temp = $temperature->f2k($degree); 
-			 print "<tr><td>$temp</td><td>kelvin</td></tr>";} 
-		 
-		 if ($scale == "kelvin") 
-		{
-			 print "<table border><tr><th colspan=2> Conversion Results</th></tr><tr><td> 
-			 $degree</td><td>kelvin</td></tr>"; 
-             
-			 $temp = $temperature->k2f($degree); 
-			 print "<tr><td>$temp</td><td>fahrenheit</td></tr>";
-             
-			 $temp = $temperature->k2c($degree); 
-			 print "<tr><td>$temp</td><td>celsius</td></tr>";} 
-		  
-	 }
-	 else
-		//print an error message if input is NULL or NaN
-		echo "<span style = \"color:red\">*Please Enter a Valid Temperature.</span>";
-}
+
+//check for input
+if (array_key_exists('degree',$_GET)){
+    $scale = $_GET['scale'];
+    $degree = $_GET['degree'];
+    $firstLength = strlen($_GET['degree']);
+
+
+//check that input is not NULL or NaN
+    if (($firstLength > 0) && (is_numeric($_GET['degree']))) {
+
+//switch statement to convert the input depending on scale and format it for output
+        switch ($scale) {
+            case "celsius":
+                $degC = number_format($degree, 2);
+                $degF = number_format((Temperature::c2f($degree)), 2);
+                $degK = number_format((Temperature::c2k($degree)), 2);
+                $degR = number_format((Temperature::c2r($degree)), 2);
+                break;
+            case "fahrenheit":
+                $degC = number_format((Temperature::f2c($degree)), 2);
+                $degF = number_format($degree, 2);
+                $degK = number_format((Temperature::f2k($degree)), 2);
+                $degR = number_format((Temperature::f2r($degree)), 2);
+                break;
+            case "kelvin":
+                $degC = number_format((Temperature::k2c($degree)), 2);
+                $degF = number_format((Temperature::k2f($degree)), 2);
+                $degK = number_format($degree, 2);
+                $degR = number_format((Temperature::k2r($degree)), 2);
+                break;
+            case "rankine":
+                $degC = number_format((Temperature::r2c($degree)), 2);
+                $degF = number_format((Temperature::r2f($degree)), 2);
+                $degK = number_format((Temperature::r2k($degree)), 2);
+                $degR = number_format($degree, 2);
+                break;
+            default;
+                $degC = "Error";
+                $degF = "Error";
+                $degK = "Error";
+                $degR = "Error";
+                break;
+         }
+
+//print a table of the conversions
+         echo 
+             "
+             <table border>
+             <tr><th colspan=2> Conversion Results</th></tr>
+             <tr><td>$degC</td><td>Celsius</td></tr>
+             <tr><td>$degF</td><td>Fahrenheit</td></tr>
+             <tr><td>$degK</td><td>Kelvin</td></tr>
+             <tr><td>$degR</td><td>Rankine</td></tr>
+             "
+             ;
+
+     } else {
+//print an error message if input is NULL or NaN
+         echo "<span style = \"color:red\">*Please Enter a Valid Temperature.</span>";
+     }
+ }
+
  ?> </div><!--end container-->
+
