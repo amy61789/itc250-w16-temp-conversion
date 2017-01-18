dbfcef457179e9eba5ad3422dc0afeda2c6dd2dc
josephwanderer <banhstopjw@gmail.com>
2017-01-17 16:42:55 -0800

    Revert to PSR-1,2 standards and static methods

--- temperatureclass.php
+++ temperatureclass.php
@@ -1,32 +1,66 @@
 <?php
+
+
 class Temperature
      {
-        function f2c($degree){
-			  
-			 return ($degree -32)*5/9;
+        public static function f2c($degree)
+        {  
+			 return ($degree - 32) * 5 / 9;
         }
      
-        function c2f($degree){
-			 
-             return $degree*9/5+32;
+        public static function c2f($degree)
+        { 
+             return ($degree * 9 / 5) + 32;
         }
         
-        function f2k($degree) { 
-            
-             return 5/9*($degree - 32) + 273;
-			 
+        public static function f2k($degree)
+        { 
+             return (($degree - 32) * 5 / 9) + 273;
         }
-        function c2k($degree) {
-            
+    
+        public static function c2k($degree)
+        {
             return $degree + 273;
         }
-        function k2c($degree) {
-            
+    
+        public static function k2c($degree) 
+        {
             return $degree - 273;
         }
-        function k2f($degree) {
-            
-            return 9/5*($degree - 273) + 32;
+    
+        public static function k2f($degree) 
+        {
+            return (($degree - 273) * 9 / 5) + 32;
         }
-     }
-?>
+    
+        public static function r2c($degree) 
+        {
+            return ($degree - 491.67) * 5 / 9;
+        }
+    
+        public static function r2f($degree) 
+        {
+            return $degree - 459.67;
+        }
+    
+        public static function r2k($degree) 
+        {
+            return $degree * 5 / 9;
+        }
+    
+        public static function c2r($degree) 
+        {
+            return ($degree * 9 / 5) + 491.67;
+        }
+    
+        public static function f2r($degree) 
+        {
+            return $degree = 459.67;
+        }
+    
+        public static function k2r($degree) 
+        {
+            return $degree * 9 / 5;
+        }
+    
+     }//end Temperature class
